a9d5d1af588bc5bf6ff57951c9f09cc7037a3f2f
Aditya Jyoti <aj.adityajyoti@gmail.com>
2024-04-07 00:20:11 +0530

    Add footer navlink

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 </script>
 
-<footer class="text-white flex flex-col gap-2 justify-center items-center py-4">
+<footer class="text-white flex flex-col gap-2 justify-center items-center py-4" id="contacts">
 	<span class="text-lg">Got any questions or feedback? We would love to hear from you!</span>
 	<div class="flex gap-4">
 		<a href="/" class="text-2xl"><i class="fa-brands fa-instagram"></i></a>
