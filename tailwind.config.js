5ef1902a1519bb88f99ad0d5b703adb0d9223b9a
Aditya Jyoti <aj.adityajyoti@gmail.com>
2024-04-06 21:46:08 +0530

    Add tailwind colourscheme [temp]

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				background: '#1B1B1B',
+				green: '#31CF9F',
+				gray: '#343434',
+				teal: '#358B9B'
+			}
+		}
 	},
 	plugins: []
 };
