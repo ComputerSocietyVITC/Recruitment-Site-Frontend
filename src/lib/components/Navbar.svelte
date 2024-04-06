9503badf989b5511b199fe26b8ed929d962f5b58
Aditya Jyoti <aj.adityajyoti@gmail.com>
2024-04-06 22:06:32 +0530

    Make navbar

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	const navItem = 'hover-1';
+</script>
+
+<nav class="flex justify-between text-white px-8 py-4">
+	<div class="flex items-center text-2xl font-bold gap-8">
+		<img src="/logo.png" alt="IEEE Comp Soc Logo" class="w-14" />
+		<span>IEEE Computer Society</span>
+	</div>
+	<div class="flex gap-12 items-center text-xl">
+		<a href="/" class={navItem}><div>Home</div></a>
+		<a href="#about" class={navItem}><div>About us</div></a>
+		<a href="#departments" class={navItem}><div>Departments</div></a>
+		<a href="#contacts" class={navItem}><div>Contacts</div></a>
+	</div>
+</nav>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+	import '../app.css';
+
+	import Navbar from '$lib/components/Navbar.svelte';
+</script>
+
+<section class="bg-background min-h-screen w-full flex flex-col">
+	<div>
+		<Navbar />
+	</div>
+	<slot />
+	<div></div>
+</section>
