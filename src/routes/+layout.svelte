f8d27bc569d5ed8cc445a6f87062e0e221f901c1
Aditya Jyoti <aj.adityajyoti@gmail.com>
2024-06-29 00:45:51 +0530

    deploy: test 3

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,21 +1,7 @@
 <script>
 	import '../app.css';
 
-	import { onMount } from 'svelte';
-
-	import { supabase } from '$lib/supabase';
-	import { user } from '$lib/stores';
-
 	import Navbar from '$lib/components/home/Navbar.svelte';
-
-	onMount(async () => {
-		const { data } = await supabase.auth.getUser();
-
-		if (data.user) {
-			let user_data = await supabase.from('User').select().eq('email', data.user.email).single();
-			$user = { id: user_data.data.id, name: user_data.data.name, email: user_data.data.email };
-		}
-	});
 </script>
 
 <section class="bg-background text-foreground min-h-screen min-w-screen font-main md:px-24 px-8">
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,8 @@
 <script lang="ts">
-	import { goto } from '$app/navigation';
-
 	import settings from '$lib/settings';
 
 	import Splash from '$lib/components/home/Splash.svelte';
-	import Departments from '$lib/components/home/Departments.svelte';
-	import Footer from '$lib/components/home/Footer.svelte';
 
-	import { user } from '$lib/stores';
 </script>
 
 <main class="flex flex-col">
