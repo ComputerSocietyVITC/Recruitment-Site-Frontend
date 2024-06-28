9a9325ca0d36fc85e485b71490282584ded8c00b
Aditya Jyoti <aj.adityajyoti@gmail.com>
2024-06-29 01:05:02 +0530

    deploy: move to static

---
+++ _app/env.js
@@ -0,0 +1,1 @@
+export const env={"PUBLIC_SUPABASE_URL":"https://ohndsucdplelvskblnni.supabase.co","PUBLIC_SUPABASE_ANON_KEY":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9obmRzdWNkcGxlbHZza2Jsbm5pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTY0NzYwNzYsImV4cCI6MjAzMjA1MjA3Nn0.x66PgR4BlqVtOHn7T6ab4jA7BLkLM-SVaTT_hSjBzZo"}
---
+++ _app/immutable/entry/start.D_XF-8u0.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.fVA4-Tgj.js";export{t as start};
